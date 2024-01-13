d98dd7ce8006e1cb406541b36b7aebf28928cd51
Ray000000 <94271266+Ray000000@users.noreply.github.com>
2024-01-13 06:56:42 +0000

    Update citation.js

--- citation.js
+++ citation.js
@@ -1,3 +1,3 @@
 document.write('<script src="https://cdn.plyr.io/3.7.8/plyr.js"></script>');
 document.write('<link rel="stylesheet" href="https://cdn.plyr.io/3.7.8/plyr.css">');
-document.write('<link rel="stylesheet" href="https://cdn.vun-video.tk/style.css">');
+document.write('<link rel="stylesheet" href=https://ogata-in-taiwan.github.io/Vun-Video-Player/style.css">');
