17c17dc920bd810b4b9edbfc0241111698a116a3
Ray000000 <94271266+Ray000000@users.noreply.github.com>
2024-01-13 09:39:43 +0000

    Update citation.js

--- citation.js
+++ citation.js
@@ -1,3 +1,3 @@
 document.write("<script src='https://cdn.plyr.io/3.7.8/plyr.js'></script>");
 document.write("<link rel='stylesheet' href='https://cdn.plyr.io/3.7.8/plyr.css'>");
-document.write("<link rel='stylesheet' href='https://ogata-in-taiwan.github.io/Vun-Video-Player/style.css'>");
+document.write("<link rel='stylesheet' href='https://ogata-in-taiwan.github.io/vun-video-player-by-plyr/style.css'>");
